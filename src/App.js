aebef81761d52a3efd4668a95d8c7a00bd92b60a
Kristian Ulvund <>
2019-10-04 16:01:54 +0200

    Add hosting

--- src/App.js
+++ src/App.js
@@ -1,56 +1,72 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import { database } from './utils'
+import React, { useEffect, useState } from "react";
+import {
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText
+} from "@material-ui/core";
+import "./App.css";
+import { database } from "./utils";
 
 const App = () => {
   const [beerList, setBeerList] = useState([]);
   const [fridgeList, setFridgeList] = useState([]);
 
   useEffect(() => {
-    database.ref('beer-list/').on('value', data => {      
-      let obj = data.val()
-      setBeerList(Object.keys(obj).map(key => {
-        return {
-          id: key,
-          name: obj[key].name,
-        }
-      }))
+    database.ref("beer-list/").on("value", data => {
+      let obj = data.val();
+      setBeerList(
+        Object.keys(obj).map(key => {
+          return {
+            id: key,
+            name: obj[key].name
+          };
+        })
+      );
     });
-    database.ref('fridge/').on('value', data => {      
-      let obj = data.val()
-      setFridgeList(Object.keys(obj).map(key => {
-        return {
-          id: key,
-          count: obj[key].count,
-        }
-      }))
+    database.ref("fridge/").on("value", data => {
+      let obj = data.val();
+      setFridgeList(
+        Object.keys(obj).map(key => {
+          return {
+            id: key,
+            count: obj[key].count
+          };
+        })
+      );
     });
-  }, [])
+  }, []);
 
-  const beerStatus = fridgeList.map(({id, count}) => {
-      const beer = beerList.find(beer => beer.id === id)
-      return {
-        id,
-        name: beer.name,
-        count
-      }
-  })
+  const beerStatus = fridgeList.map(({ id, count }) => {
+    const beer = beerList.find(beer => beer.id === id);
+    return {
+      id,
+      name: beer.name,
+      count
+    };
+  });
 
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-            {
-              beerStatus.map(e => 
-                <div>
-                  <p>{e.name}</p>
-                  <p>{e.count}</p>
-                </div>
-            )}
-        </div>
-      </header>
+      <List style={{ color: "black" }}>
+        {beerStatus.map(e => (
+          <ListItem
+            key={e.id}
+            style={{ marginBottom: "10px", backgroundColor: "white" }}
+          >
+            <ListItemAvatar>
+              <Avatar
+                alt="Drinks"
+                src="https://www.coca-cola.no/content/dam/journey/no/no/Global/products/coca-cola-uten-sukker-595x334.jpg"
+              />
+            </ListItemAvatar>
+            <ListItemText primary={e.name} secondary={`Antall: ${e.count}`} />
+          </ListItem>
+        ))}
+      </List>
     </div>
-    )
+  );
 };
 
 export default App;
